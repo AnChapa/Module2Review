b9cbb33cd1c7c79bb10f5b08b2f14d20c591bb29
Antonio Chapa <antonio.luis.chapa@gmail.com>
2024-01-25 11:59:50 -0600

    Created Student class

---
+++ Module2Review/Student.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module2Review
+{
+    internal class Student
+    {
+        private string Name {  get; set; }
+        private int Id { get; set; }
+        private List<double> Grades { get; set; }
+
+        public Student()
+        {
+            Grades = new List<double>();
+        }
+
+    }
+}
